830fd3c1a4e047723331b62fed662526681cd27e
Alexis Indick <aindick@oswego.edu>
2021-02-07 10:19:29 -0500

    Fixing up some code involving the socket.

--- src/P1RTT/TCPServer.java
+++ src/P1RTT/TCPServer.java
@@ -10,11 +10,10 @@ public class TCPServer {
             ServerSocket socket = new ServerSocket(port);
             //setSoTimeout throws a SocketTimeout Exception when time is reached.
             socket.setSoTimeout(2000);
-            Socket a;
 
             for(;;){
                 try{
-                    a = socket.accept();
+                    Socket a = socket.accept();
                     System.out.println("Works on: " + port);
                     break;
                     //Catches if the socket times out.
@@ -23,7 +22,7 @@ public class TCPServer {
                 }
 
             }
-            //Catch exception that will be thrown when something goes wrong and will exit the program
+            //Catch exception that will be thrown when something goes wrong and will exit the program.
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(-1);
