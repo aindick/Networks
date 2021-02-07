3b3fcb3e19e5151717df71d2eac7b95bd1880227
Alexis Indick <aindick@oswego.edu>
2021-02-07 09:59:20 -0500

    I have added an I/O Exception and a Server Socket along with a socket timeout.

--- src/P1RTT/TCPServer.java
+++ src/P1RTT/TCPServer.java
@@ -5,6 +5,14 @@ public class TCPServer {
     static final int port = 2740;
 
     public static void main(String[] args){
-        
+        //Establishing a new socket that connects to my port.
+        try{
+            ServerSocket socket = new ServerSocket(port);
+            //setSoTimeout throws a SocketTimeout Exception when time is reached.
+            socket.setSoTimeout(2000);
+            //Catch exception that will be thrown when something goes wrong.
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
