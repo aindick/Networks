b0b5901065138630f7bc29c3e6eb30e2d4a96f60
Alexis Indick <aindick@oswego.edu>
2021-02-10 10:12:34 -0500

    Added an ArrayList for average kilobytes and a new socket.

--- src/P1RTT/TCPClient.java
+++ src/P1RTT/TCPClient.java
@@ -1,7 +1,20 @@
 package P1RTT;
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
+
 public class TCPClient {
     static final int port = 2740;
     static final String host = "129.3.149.47";
+
+    public static void main(String[] args){
+        ArrayList<Long> averageKB = new ArrayList<>();
+        try{
+            Socket socket = new Socket(host, port);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
