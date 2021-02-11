29d33561d69e796bd75cd01dcf3a70e0bc84c49e
Alexis Indick <aindick@oswego.edu>
2021-02-11 12:13:09 -0500

    Server fragmentation done! Now onto the client.

--- src/P1RTT/FragmentTCPClient.java
+++ src/P1RTT/FragmentTCPClient.java
@@ -1,4 +1,32 @@
 package P1RTT;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
 
 public class FragmentTCPClient {
+    static final String host = "129.3.20.62";
+    static final int port = 2740;
+
+    static final int kB = 1024;
+    static final int mB = kB * 1024;
+
+    public static void main(String[] args){
+        ArrayList<Long> averageOne = new ArrayList<>();
+        ArrayList<Long> averageTwo = new ArrayList<>();
+        ArrayList<Long> averageFour = new ArrayList<>();
+    }
+
+    private static double average(ArrayList<Long> values) {
+        int r = values.size();
+        double s = 0;
+        for (Long v : values) {
+            if(v != 0) {
+                s += (v / 1000000);
+            }
+        }
+        s /= r;
+        return s;
+    }
 }
--- src/P1RTT/FragmentTCPServer.java
+++ src/P1RTT/FragmentTCPServer.java
@@ -1,5 +1,69 @@
 package P1RTT;
 
-public class FragmentTCPServer {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
 
+public class FragmentTCPServer {
+    static final int port = 2740;
+
+    public static void main(String[] args){
+        Socket client;
+        //Establishing a new socket that connects to my port.
+        try {
+            ServerSocket socket = new ServerSocket(port);
+            //setSoTimeout throws a SocketTimeout Exception when time is reached.
+            socket.setSoTimeout(2000);
+
+            for (; ; ) {
+                try {
+                    client = socket.accept();
+                    System.out.println("Works on: " + port);
+                    break;
+                    //Catches if the socket times out.
+                } catch (SocketTimeoutException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            //This is for reading primitive Java data types(Data) and writing data (Output). Input = reading bytes.
+            OutputStream ops = client.getOutputStream();
+            DataOutputStream dops = new DataOutputStream(ops);
+            InputStream ins = client.getInputStream();
+            DataInputStream di = new DataInputStream(ins);
+            int size = 0;
+            for (;; ) {
+                try {
+                    int messages = di.readInt();
+                    for(int k = 0; k < messages; k++){
+                        size = di.readInt();
+                        byte[] byt = new byte[size];
+                        di.readFully(byt);
+                    }
+
+                } catch (EOFException e) {
+                    System.out.println("Done on Port: " + port);
+                    break;
+
+                }
+                dops.writeChar('a');
+                System.out.println("Received:" + (size/1024));
+            }
+            //Closing up the scanner
+            ops.close();
+            ins.close();
+            di.close();
+            client.close();
+            socket.close();
+
+
+
+            //Catch exception that will be thrown when something goes wrong and will exit the program.
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+    }
 }
